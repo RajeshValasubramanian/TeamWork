f9bb072005fae2f4764a1eba1605527ec6f4305a
RbalajiU0 <balaji.udayasankar@relevantz.com>
2025-03-14 16:45:37 +0530

    First Commit

---
+++ calculator.cs
@@ -0,0 +1,13 @@
+class functions{
+    
+}
+class Tasks{
+    public static void Main(string[] args){
+        functions obj=new functions();
+        int a=10,b=10;
+        obj.add(a,b);//Balaji
+        obj.sub(a,b);//Aashik
+        obj.mul(a,b);//Bangaru
+        obj.div(a,b);//Sandhiya
+    }
+}
